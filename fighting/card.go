f4e214b6624b6c1a1e5f322fab1803d8d8434a02
evgeniy.bayanov <e.bayanov@skyeng.ru>
2022-05-22 18:42:59 +0700

    [fightning] Add card race

--- fighting/card.go
+++ fighting/card.go
@@ -2,17 +2,26 @@ package main
 
 import "math/rand"
 
+type CardRace uint8
+
+const (
+	Common CardRace = iota
+	Murloc          = 1
+	Elf             = 2
+	Orc             = 3
+)
+
 type Card struct {
 	attack int
 	health int
 	status string
+	race   int
 }
 
 func fillCards(cards *[5]Card) {
 	for i := 0; i < len(cards); i++ {
-		cards[i].attack = rand.Intn(10) + 1
-		cards[i].health = rand.Intn(10) + 1
-		cards[i].status = "alive"
+		var card = Card{rand.Intn(10) + 1, rand.Intn(10) + 1, "alive", rand.Intn(4)}
+		cards[i] = card
 	}
 }
 
